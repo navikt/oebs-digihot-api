950e1284db5df59a7e9e21b3f0c5e27851b59ebd
bougrNAV <abid.bougroug@nav.no>
2025-08-06 12:48:19 +0200

    Feilrettelse i controller sf

--- src/main/java/no/nav/oebs/digihot/api/digihot/serviceforesporsel/v1/ServiceforesporselController.java
+++ src/main/java/no/nav/oebs/digihot/api/digihot/serviceforesporsel/v1/ServiceforesporselController.java
@@ -34,7 +34,7 @@ public class ServiceforesporselController {
 		this.service = service;
 	}
 
-	@Unprotected
+	@Protected
 	@GetMapping(path = "/serviceforesporsel")
 	@DigihotSwagger
 	public String finnServiceforesporselTransaksjoner(
